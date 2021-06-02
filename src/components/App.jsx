ca1c42a3707fa580006cd4b0e10f2d1ec2ae978f
AYAN CHAKRABORTY <1905741@kiit.ac.in>
2021-06-02 16:25:18 +0530

    Header part is complete

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "./Navbar";
 import Cover from "./Cover";
+import CoverContent from "./CoverContent.jsx";
 function App(){
     return <div>
         <Navbar />
         <Cover />
+        <CoverContent />
         <div className="white"></div>
     </div>;
 }
---
+++ src/components/CoverContent.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function CoverContent(){
+    return <div className="cover-content">
+        <h1>Endless entertainment and knowledge</h1>
+        <p>Read or listen anytime, anywhere.</p>
+        <button type="button" class="btn btn-light btn-lg">Read free for 30 days</button>
+        <p className="p-below">Only $8.99/month after. Cancel anytime.</p>
+    </div>
+}
+
+export default CoverContent;
