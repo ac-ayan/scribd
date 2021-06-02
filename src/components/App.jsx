ba0501b4a325250e83ad3ca544604f0df5a17f59
AYAN CHAKRABORTY <1905741@kiit.ac.in>
2021-06-02 20:56:56 +0530

    Adding Quotes Section final

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,12 +3,14 @@ import Navbar from "./Navbar";
 import Cover from "./Cover";
 import CoverContent from "./CoverContent.jsx";
 import Subscription from "./Subscription";
+import SubQuote from "./SubQuote";
 function App(){
     return <div>
         <Navbar />
         <Cover />
         <CoverContent />
         <Subscription />
+        <SubQuote />
     </div>;
 }
 
---
+++ src/components/SubQuote.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function SubQuote(){
+    return <div className="sub-quote">
+        <img className="quote-img" src="../assets/subscription-section-quote-icon.png"></img>
+        <p className="main-para">Scribd is overall the best and most convenient deal for online reading.</p>
+        <div>
+            
+            <p className="sub-para">Business Insider</p>
+        </div>
+    </div>
+}
+export default SubQuote;
